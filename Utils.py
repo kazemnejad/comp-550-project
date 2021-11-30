9a262d971fed6564d08870e755e8e987f5cd3494
Dimitri-Sinodinos <dimitrios.sinodinos@mail.mcgill.ca>
2021-11-30 14:01:49 -0500

    Testing HuggingFace Transformers

---
+++ Utils.py
@@ -0,0 +1,109 @@
+import torch
+from torch import nn
+from torch.optim import Adam
+from torch.optim import Adam
+from tqdm import tqdm
+import numpy as np
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, df, tokenizer):
+
+        self.labels = [label for label in df['labels']]
+        self.texts = [tokenizer(text, 
+                               padding='max_length', max_length = 512, truncation=True,
+                                return_tensors="pt") for text in df['text']]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
+
+def train(model, train_data, learning_rate, epochs, tokenizer):
+
+    train = Dataset(train_data, tokenizer)
+
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=2, shuffle=True)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr= learning_rate)
+
+    if use_cuda:
+
+            model = model.cuda()
+            criterion = criterion.cuda()
+
+    for epoch_num in range(epochs):
+
+            total_acc_train = 0
+            total_loss_train = 0
+
+            for train_input, train_label in tqdm(train_dataloader):
+
+                train_label = train_label.to(device).long()
+                mask = train_input['attention_mask'].to(device)
+                input_id = train_input['input_ids'].squeeze(1).to(device)
+
+                output = model(input_id, mask)
+                
+                batch_loss = criterion(output, train_label)
+                total_loss_train += batch_loss.item()
+                
+                acc = (output.argmax(dim=1) == train_label).sum().item()
+                total_acc_train += acc
+
+                model.zero_grad()
+                batch_loss.backward()
+                optimizer.step()
+            
+            print(
+                f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+                | Train Accuracy: {total_acc_train / len(train_data): .3f}')
+                
+def evaluate(model, test_data, tokenizer):
+
+    test = Dataset(test_data, tokenizer)
+
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=2)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if use_cuda:
+
+        model = model.cuda()
+
+    total_acc_test = 0
+    with torch.no_grad():
+
+        for test_input, test_label in test_dataloader:
+
+              test_label = test_label.to(device)
+              mask = test_input['attention_mask'].to(device)
+              input_id = test_input['input_ids'].squeeze(1).to(device)
+
+              output = model(input_id, mask)
+
+              acc = (output.argmax(dim=1) == test_label).sum().item()
+              total_acc_test += acc
+    
+    print(f'Test Accuracy: {total_acc_test / len(test_data): .3f}')
---
+++ test_transformers.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from transformers import BertTokenizer
+from Classifiers import BertClassifier
+from Utils import train, evaluate
+from torchtext.datasets import AG_NEWS
+
+train_iter, test_iter = AG_NEWS(root='.data',split=('train','test'))
+
+train_data = []
+for sample in train_iter:
+    train_data.append([sample[1],sample[0]])
+
+train_df = pd.DataFrame(train_data)
+train_df.columns = ["text", "labels"]
+
+test_data = []
+for sample in test_iter:
+    test_data.append([sample[1],sample[0]])
+
+test_df = pd.DataFrame(test_data)
+test_df.columns = ["text", "labels"]
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+             
+EPOCHS = 5
+model = BertClassifier()
+LR = 1e-6
+              
+train(model, train_df, LR, EPOCHS, tokenizer)
+
+evaluate(model, test_iter, tokenizer)
